63026f2b7dae4bbebbe7d7a260a1757079799904
Balamurugan1962 <145089896+Balamurugan1962@users.noreply.github.com>
2025-04-04 00:45:15 +0530

    Update main.py

--- api/main.py
+++ api/main.py
@@ -2,7 +2,7 @@ from flask import Flask,request,jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-cors = CORS(app,origins=["*"])
+cors = CORS(app,origins=["http://localhost:5173"])
 
 db = {
     "Bala" : {
