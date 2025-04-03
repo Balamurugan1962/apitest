f89b9481d8a848ff2d669e5963cffc81b45ca2a3
Balamurugan1962 <145089896+Balamurugan1962@users.noreply.github.com>
2025-04-04 00:02:20 +0530

    Added Deatils

--- api/main.py
+++ api/main.py
@@ -1,10 +1,31 @@
-from flask import Flask
+from flask import Flask,request
 
 app = Flask(__name__)
+
+db = {
+    "Bala" : {
+        "Age" : 19,
+        "Dept" : "CSE"
+    },
+    "Yukii" : {
+        "Age" : 19,
+        "Dept" : "CSE"
+    },
+    "Vasant" : {
+        "Age" : 19,
+        "Dept" : "Civil"
+    }
+}
 
 @app.route('/')
 def start():
     return {"message": "Hello By Flask!"}
+
+@app.route('/Details',method = ['GET','POST'])
+def details():
+    content = request.get_json(silent=True)
+    return content
+
 
 if __name__ == "__main__":
     app.run(debug=True)
