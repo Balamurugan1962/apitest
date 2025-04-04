a565d691a098f575385eb588657cb66d56e39ffa
Balamurugan1962 <145089896+Balamurugan1962@users.noreply.github.com>
2025-04-04 20:50:27 +0530

    Update main.py

--- api/main.py
+++ api/main.py
@@ -1,12 +1,15 @@
 from flask import Flask,request,jsonify
 from flask_cors import CORS
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 from llm_api_call import chat
 
 app = Flask(__name__)
 cors = CORS(app,origins='*')
 api = Api(app)
 
+ChatWithAi_post_args = reqparse.RequestParser()
+ChatWithAi_post_args.add_argument("Type",type=str,help="Either user or system")
+ChatWithAi_post_args.add_argument("Message",type=str,help="Messege to send llm's",required = True)
 
 db = {
     "Bala" : {
@@ -23,23 +26,24 @@ db = {
     }
 }
 
-class Hello(Resource):
+class ChatWithAi(Resource):
     def post(self):
-        content = request.get_json()
-        return jsonify(content)
+        args = ChatWithAi_post_args.parse_args()
+        reply = chat(args["Message"])
+        return jsonify({"reply" : reply})
 
     def get(self):
         return jsonify({"Hello":"Hiii"})
     
 
-api.add_resource(Hello,'/GetInfo')
+api.add_resource(ChatWithAi,'/ChatWithAi')
 
 
 @app.route('/')
 def start():
     message = chat("Hi")
     print(message)
-    return {"LLM Replay": message}
+    return jsonify({"LLM Replay": message})
 
 @app.route('/Details',methods = ['GET','POST'])
 def details():
@@ -48,4 +52,4 @@ def details():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0",port="5001")
